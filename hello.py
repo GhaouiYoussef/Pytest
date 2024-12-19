e9bd610ee71418436bb8f9c4a7a5768aa5a1f037
youssef ghaoui <120917144+GhaouiYoussef@users.noreply.github.com>
2024-12-19 16:37:24 +0000

    adding base files

---
+++ hello.py
@@ -0,0 +1,8 @@
+def more_hello():
+    return "Hello again!"
+
+
+# var = 1
+# var = var  # bad code
+
+# foo =
---
+++ test_hello.py
@@ -0,0 +1,6 @@
+from hello import more_hello
+
+
+def test_more_hello():
+
+    assert more_hello() == "Hello again!"
